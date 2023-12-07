62043ecffb4eee00e98b8568c3a32dcbfb90a71f
ohseungmin <ohmin2886@gmail.com>
2023-12-07 11:40:27 +0900

    빈 객체만 생성해도 데이터 타입은 객체라고 판명된다.

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var staticObject = {
   name: "static object",
   explain : "미리, 혹은 이미 작성된 객체"
 };
-console.dir(typeof(staticObject)); // 'object
+console.dir(typeof(staticObject)); // 'object'
 console.log(staticObject.name); // static object
 console.log(staticObject.explain); // 미리, 혹은 이미 작성된 객체
+
+var dynamicObject = {};
+dynamicObject.name = "dynamic object";
+dynamicObject.explain = "동적으로 생성된 객체";
+console.dir(typeof(dynamicObject)) // 'object'
